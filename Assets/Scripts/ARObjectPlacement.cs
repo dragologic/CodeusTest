089694628620881325a96ff8a0aecd0e7af77c16
dragologic <dragologic@gmail.com>
2021-02-02 19:12:08 +0100

    weather

---
+++ Assets/Scripts/ARObjectPlacement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+
+public class ARObjectPlacement : MonoBehaviour
+{
+    public GameObject objectToPlace;
+    public GameObject placementIndicator;
+
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager raycastManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+}
---
+++ Assets/Scripts/OpenWeatherConnector.cs
@@ -0,0 +1,67 @@
+﻿using SimpleJSON;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+public class OpenWeatherConnector : MonoBehaviour
+{
+    string AppId = "2593e913cfe95f2067bde60897e16dca";
+    string latitude = "43.321126219625036";
+    string longitude = "21.89583076867475";
+    public InputField city;
+    public InputField weatherDescription;
+    public InputField temp;
+    public float temp_min;
+    public float temp_max;
+    public float rain;
+    public int clouds;
+    public float wind;
+
+    public GameObject popupScreen;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        popupScreen.SetActive(false);
+    }
+    public void getWeatherData()
+    {
+        StartCoroutine(getData());
+    }
+    public IEnumerator getData()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(string.Format("api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&units=metric&appid={2}", latitude,longitude,AppId));
+        yield return request.SendWebRequest();
+        if (request.error == null || request.error == "")
+        {
+            setWeatherAttributes(request.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log("Error: " + request.error);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setWeatherAttributes(string jsonString)
+    {
+        var weatherJson = JSON.Parse(jsonString);
+        city.text = weatherJson["name"].Value;
+        weatherDescription.text = weatherJson["weather"][0]["description"].Value;
+        temp.text = weatherJson["main"]["temp"].AsFloat.ToString();
+
+        temp_min = weatherJson["main"]["temp_min"].AsFloat;
+        temp_max = weatherJson["main"]["temp_max"].AsFloat;
+        rain = weatherJson["rain"]["3h"].AsFloat;
+        clouds = weatherJson["clouds"]["all"].AsInt;
+        wind = weatherJson["wind"]["speed"].AsFloat;
+
+        popupScreen.SetActive(true);
+    }
+}
